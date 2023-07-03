416d943540793cf4575f2cedbaa56123d658bf11
shreyash raut <121212284+shreyash8080@users.noreply.github.com>
2023-07-03 17:28:06 +0530

    Add files via upload

---
+++ ass2.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+# coding: utf-8
+Number 1 -
+
+Question -
+What are the two values of the Boolean data type? How do you write them?
+
+Answer -
+True and False, using capital T and F, with the rest of the word in lowercase
+
+Number 2 -
+
+Question -
+What are the three different types of Boolean operators?
+
+Answer -
+and, or, and not
+
+
+Number 3 -
+
+Question -
+Write out the truth tables of each Boolean operator (that is, every possible combination of Boolean values for the operator and what they evaluate to).
+
+Answer -
+True and True is True.
+True and False is False.
+False and True is False.
+False and False is False.
+True or True is True.
+True or False is True.
+False or True is True.
+False or False is False.
+not True is False.
+not False is True.
+
+
+Number 4 -
+
+Question -
+What do the following expressions evaluate to?
+
+(5 > 4) and (3 == 5)
+not (5 > 4)
+(5 > 4) or (3 == 5)
+not ((5 > 4) or (3 == 5))
+(True and True) and (True == False)
+(not False) or (not True)
+Answer -
+False
+False
+True
+False
+False
+True
+
+
+Number 5 -
+
+Question -
+What are the six comparison operators?
+
+Answer -
+The six different types of reference operators are : ==, !=, <, >, <=, and >=.
+
+
+
+Number 6 -
+
+Question -
+What is the difference between the equal to operator and the assignment operator?
+
+Answer -
+== is the equal to operator that compares two values and evaluates to a Boolean, while = is the assignment operator that stores a value in a variable
+
+Number 7 -
+
+Question -
+Identify the three blocks in this code:
+
+Number 7 -
+
+Question -
+Identify the three blocks in this code:
+
+spam = 0
+if spam == 10:
+    print('eggs')
+    if spam > 5:
+        print('bacon')
+    else:
+        print('ham')
+    print('spam')
+print('spam')
+spam
+- Answer - >The three blocks are everything inside the if statement
+
+
+Number 8 -
+
+Question -
+Explain what a condition is and where you would use one.
+
+Answer -
+A condition is an expression used in a flow control statement that evaluates to a Boolean value.
+# In[ ]:
+
+
+
+
