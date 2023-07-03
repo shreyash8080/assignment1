596d111e0762eb8edaf4b2e6d19397c89678e56b
shreyash raut <121212284+shreyash8080@users.noreply.github.com>
2023-07-03 17:17:48 +0530

    Add files via upload

---
+++ ass. 1.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# coding: utf-8
+1 In the below elements which of them are values or an expression? eg:- values can be integer or string and expressions will be mathematical operators.
+
+'hello'
+-87.8
+/
+6
+Answer -
+Arithmatic Operator
+String Value
+Floating-point Value
+Arithmatic Operator
+Arithmatic Operator
+Arithmatic Operator
+Integer
+
+2 Question -
+What is the difference between string and variable?
+
+Answer -
+Variable - spam
+String - 'spam'
+Strings always start and end with quotes
+
+3 Describe three different data types.
+
+Answer -
+Integers,
+floating-point numbers
+strings.
+
+4 What is an expression made up of? What do all expressions do?
+
+Answer -
+We can say an expression is a combination of values, variables, operators There are three kinds of expressions: An arithmetic expression evaluates to a single arithmetic value. A character expression evaluates to a single value of type character. A logical or relational expression evaluates to a single logical value. All expressions evaluate (that is, reduce) to a single value.
+
+5 This assignment statements, like spam = 10. What is the difference between an expression and a statement?
+
+Answer -
+An expression evaluates to a single value. A statement does not
+
+6 After running the following code, what does the variable bacon contain?
+
+bacon = 22
+bacon + 1
+Answer -
+The bacon variable is set to 22. The bacon + 1 expression does not reassign the value in bacon (that would need an assignment statement: bacon = bacon + 1
+
+7 What should the following two expressions evaluate to?
+
+'spam' + 'spamspam'
+'spam' * 3
+Answer -
+Both expressions evaluate to the string 'spamspamspam
+
+8 Why is it that eggs is a true variable name but 100 is not?
+
+Answer -
+Variable names cannot begin with a number.
