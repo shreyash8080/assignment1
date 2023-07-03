52a2cc5d86a4f6f30cf8602a0a3585747adb6d8b
shreyash raut <121212284+shreyash8080@users.noreply.github.com>
2023-07-03 18:16:01 +0530

    Add files via upload

---
+++ assignment  9.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+# coding: utf-8
+Number 1 -
+
+Question -
+To what does a relative path refer?
+
+Answer -
+Relative paths are relative to the current working directory.
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+Number 2 -
+
+Question -
+What does an absolute path start with your operating system?
+
+Answer -
+Absolute paths start with the root folder, such as / or C:. An absolute path always contains the root elements and the complete list of directories to locate the specific file or folder. All the information required to locate the file or folder is available in the absolute path.
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+Number 3 -
+
+Question -
+What do the functions os.getcwd() and os.chdir() do?
+
+Answer -
+The os.getcwd() function returns the current working directory. The os.chdir() function changes the current working directory.
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+Number 4 -
+
+Question -
+What are the . and .. folders?
+
+Answer -
+The . folder is the current folder, and .. is the parent folder.
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+Number 5 -
+
+Question -
+In C:\bacon\eggs\spam.txt, which part is the dir name, and which part is the base name?
+
+Answer -
+C:\bacon\eggs is the dir name, while spam.txt is the base name.
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+Number 6 -
+
+Question -
+What are the three “mode” arguments that can be passed to the open() function?
+
+Answer -
+The string 'r' for read mode, 'w' for write mode, and 'a' for append mode
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+Number 7 -
+
+Question -
+What happens if an existing file is opened in write mode?
+
+Answer -
+An existing file opened in write mode is erased and completely overwritten.
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+Number 8 -
+
+Question -
+How do you tell the difference between read() and readlines()?
+
+Answer -
+The read() method returns the file's entire contents as a single string value. The readlines() method returns a list of strings, where each string is a line from the file's contents.
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+Number 9 -
+
+Question -
+What data structure does a shelf value resemble?
+
+Answer -
+A shelf value resembles a dictionary value; it has keys and values, along with keys() and values() methods that work similarly to the dictionary methods of the same names.
