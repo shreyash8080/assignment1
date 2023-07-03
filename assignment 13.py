214aea03fba8cd6dec566ea8663891c679de6c6e
shreyash raut <121212284+shreyash8080@users.noreply.github.com>
2023-07-03 18:34:12 +0530

    Add files via upload

---
+++ assignment 13.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# coding: utf-8
+Number 1 -
+
+Question -
+What advantages do Excel spreadsheets have over CSV spreadsheets?
+
+Answer -
+In Excel, spreadsheets can have values of datatypes other than strings; cells can have different fonts, sizes, or color settings; cells can have varying widths and heights; adjecent cells can be merged; and we can embed images and charts.
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+Number 2 -
+
+Question -
+What do you pass to csv.reader() and csv.writer() to create reader and writer objects?
+
+Answer -
+We pass a file object, obtained from call to open().
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+Number 3 -
+
+Question -
+What modes do File objects for reader and writer objects need to be opened in?
+
+Answer -
+File objects need to be opened in read-binary ('rb') for reader objects and write-binary ('wb') fro writer objects.
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+Number 4 -
+
+Question -
+What method takes a list argument and writes it to a CSV file?
+
+Answer -
+The writerrow() method.
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+Number 5 -
+
+Question -
+What do the keyword arguments delimiter and line terminator do?
+
+Answer -
+The delimeter argument change the string used to separate cells in a row. The lineterminator argument changes the string used to separate rows.
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+Number 6 -
+
+Question -
+What function takes a string of JSON data and returns a Python data structure?
+
+Answer -
+json.loads()
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+Number 7 -
+
+Question -
+What function takes a Python data structure and returns a string of JSON data?
+
+Answer -
+json.dumps()
